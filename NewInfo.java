d891eee7976fffb742ab7c5f9348c373a59f5ea2
Filipe Feitosa <122735570+FilipeFeitosa89@users.noreply.github.com>
2023-06-14 15:08:16 -0300

    Update NewInfo.java

--- NewInfo.java
+++ NewInfo.java
@@ -4,33 +4,26 @@ public class NewInfo {
     
     public static void main (String [] args) {
         //Declaração de variáveis
-        int idade;
+        int idade; //armazena o valor da idade
         
         System.out.println("+++ Bem-vindo a NewInfo +++");
-        System.out.println("Para que você seja encaminhado a fila correta, "
-                + "informe sua idade corretamente.");
+        System.out.println("Para que você seja encaminhado a fila correta, " + "informe sua idade corretamente.");
         
         //entrada de dados
         try {
-            idade = Integer.parseInt(JOptionPane.showInputDialog("Por favor, "
-                    + "informe sua idade:"));
+            idade = Integer.parseInt(JOptionPane.showInputDialog("Por favor, " + "informe sua idade:"));
 
             if (idade > 80){
-                JOptionPane.showMessageDialog(null, "Por favor,"
-                        + " dirija-se a fila prioritária 80+.");
+                JOptionPane.showMessageDialog(null, "Por favor," + " dirija-se a fila prioritária 80+.");
             } else {
                 if (idade >= 60) {
-                    JOptionPane.showMessageDialog(null, "Por favor,"
-                            + " dirija-se a fila prioritária.");
+                    JOptionPane.showMessageDialog(null, "Por favor," + " dirija-se a fila prioritária.");
                 } else {
-                    JOptionPane.showMessageDialog(null, "Por favor,"
-                            + "dirija-se a fila comum.");
+                    JOptionPane.showMessageDialog(null, "Por favor," + "dirija-se a fila comum.");
                 }
             } //fim do if
         } catch (NumberFormatException e) {
-            JOptionPane.showMessageDialog (null, "Informe a idade "
-                    + "corretamente", "ATENÇÃO", 
-JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog (null, "Informe a idade " + "corretamente", "ATENÇÃO", JOptionPane.ERROR_MESSAGE);
         } //fim do try-catch
     } //fim do main
 } // fim da classe
